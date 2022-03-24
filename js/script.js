67a0b97f030af2b595c89b1e9f9bc51c30bad640
Davide Chon <57179442+davidechon@users.noreply.github.com>
2022-03-24 16:21:11 +0100

    js wip

--- js/script.js
+++ js/script.js
@@ -5,11 +5,14 @@
 //   C -->|salva il valore di C nella variabile totCostoKm| D{alert>Quanti anni hai?}
 
 //   D -->|salva il valore di D nella variabile eta| E[18<eta<65]
+
 //   D -->|se minore o uguale a 18 applica il 20% di sconto| F[<=18]
+
 //   D -->|se maggiore o uguale a 65 applica il 40% di sconto| G[>=65]
-//   E -->|salva il valore di E nella variabile bigliettoIntero| H[promt>B+E]
-//   F -->|salva il valore di F nella variabile under18| I[promt>B-F]
-//   G -->|salva il valore di G nella variabile over65| L[promt>B-G]
+
+//   E -->|salva il valore di E nella variabile bigliettoIntero| H[promt>C+E]
+//   F -->|salva il valore di F nella variabile under18| I[promt>C-F]
+//   G -->|salva il valore di G nella variabile over65| L[promt>C-G]
 //   H -->|salva il valore di H nella variabile totale| M[promt>Il prezzo del biglietto ammoanta a € H]
 //   I -->|salva il valore di I nella variabile totale| M[promt>Il prezzo del biglietto ammoanta a € I]
 //   L -->|salva il valore di L nella variabile totale| M[promt>Il prezzo del biglietto ammoanta a € L]
@@ -19,7 +22,7 @@
 
 const costoKm = 0.21;
 const under18 = 0.20;
-const bigliettoIntero = 100;
+const bigliettoIntero = 1;
 const over65 = 0.40;
 
 // let kmStimati = ;
@@ -35,8 +38,16 @@ console.log("Totale costo kilometrico: ", totCostoKm);
 let eta = prompt('Quanti anni hai?');
 console.log("Età in anni:",eta);
 
-if(eta<=18||eta<=65){
-  console.log("Biglietto ridotto");
+// if(eta<=18&&eta<=65){
+//   console.log("Biglietto ridotto");
+// } else{
+//   console.log("Biglietto intero");
+// }
+
+if(eta<=18){
+  console.log("Biglietto ridotto. Il prezzo del biglietto è:", totCostoKm*under18);
+} else if(eta>=65){
+  console.log("Biglietto ridotto. Il prezzo del biglietto è:", totCostoKm*over65);
 } else{
-  console.log("Biglietto intero");
+  console.log("Biglietto intero. Il prezzo del biglietto è:", totCostoKm);
 }
